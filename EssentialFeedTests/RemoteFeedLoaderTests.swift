22c93f0413f6c74427b50744254c8cdb99e6419a
Shreya Pallan <shreya.90@gm>
2021-03-12 13:19:27 +0530

    on calling load in remotefeedloader data is requested from the client

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+import XCTest
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
+}
+
+class HTTPClient {
+   static let shared = HTTPClient()
+    
+    private init() {}
+    var requestedURL : URL?
+}
+
+class RemoteFeedLoaderTests :  XCTestCase {
+    //input is a URL that we don't have yet. To request data from a URL we will need a colaborator. URLSession, AFNetwork.... to actually make the request for us. an HTTPCLient. So the test is that when we (client) call load we will make an HTTP request with that HttpCLient.
+    
+    func test_init_doesNotRequestDataFromURL() {
+        
+        let client = HTTPClient.shared
+        //system under test is the remote feed loader
+        _ = RemoteFeedLoader()
+        
+        
+        //sut.load()  //execute load command
+        
+        XCTAssertNil(client.requestedURL)  // we didnt make a load request so url should be nil
+    }
+    
+    
+    func test_load_rwquestDataFromURL(){
+        
+        
+        //Arrange -  Given a client and sut
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        //Act -  When we invoke load
+        sut.load()
+        
+        //Assert - assert that a url request was initiated in the client
+        XCTAssertNotNil(client.requestedURL)
+        
+        
+        //But why does HttpClient need to be a singleton... there needs to be a good reason
+    }
+}
