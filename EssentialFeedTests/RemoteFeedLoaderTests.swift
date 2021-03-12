57bd783deef056d663f7073dc8875148bb0fd44b
Shreya Pallan <shreya.90@gm>
2021-03-12 13:02:28 +0530

    RemoteTestLoader does not load data upon creation

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,9 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Shreya Pallan on 11/03/21.
+//  Copyright © 2021 Shreya Pallan. All rights reserved.
+//
+
+import Foundation
