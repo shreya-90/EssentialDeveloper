fc541d93c695158778a43d30ac24ebf705ce4452
Shreya Pallan <shreya.90@gm>
2021-04-06 15:09:21 +0530

    Calls resume on data task on creation

--- EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
@@ -16,10 +16,10 @@ class URLSessionHttpCLient {
         self.session = session
     }
     
-    func get(from url  : URL){
+   private func get(from url  : URL){
         session.dataTask(with: url) { (_, _, _) in
             
-        }
+        }.resume()
     }
 }
 
@@ -34,22 +34,48 @@ class URLSessionHTTPClientTests : XCTestCase {
         
         XCTAssertEqual(session.receivedURLs, [url])
     }
+    
+    func test_get_resumesDataTask(){
+        
+       let url = URL(string: "http://any-url.com")!
+       let session = URLSessionSpy()
+       let task = URLSessionDataTaskSpy()
+        
+        //You need to tell this session to return THIS DataTaskSpy for a particular URL -  stub that behavious
+       session.stub(url: url, task:task)
+       let sut = URLSessionHttpCLient(session:session)
+       
+       sut.get(from : url)
+       
+        XCTAssertEqual(task.resumeCallCount,1)
+    }
 }
 
+// MARK :- Helper Methods
 
 private class URLSessionSpy : URLSession {
     var receivedURLs = [URL]()
     
+    private var stubs = [URL:URLSessionDataTask]()
+    
+    func stub(url : URL , task : URLSessionDataTask){
+        stubs[url] = task
+    }
     
     override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
         receivedURLs.append(url)
         
-        return FakeURLSessionDataTask()
+        return stubs[url] ?? FakeURLSessionDataTask()
     }
     
 }
 
-private class FakeURLSessionDataTask : URLSessionDataTask {
-    
+private class FakeURLSessionDataTask : URLSessionDataTask {}
+private class URLSessionDataTaskSpy : URLSessionDataTask {
+    var resumeCallCount : Int = 0
+       
+       override func resume() {
+           resumeCallCount += 1
+       }
 }
 
