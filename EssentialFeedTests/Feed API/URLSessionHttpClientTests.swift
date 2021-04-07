5b0284c8f58e64fd196cc2e70377824a3e789329
Shreya Pallan <shreya.90@gm>
2021-04-07 12:09:48 +0530

    Add nil data and nil response to the request error test case to make the setup more explicit

--- EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHttpClientTests.swift
@@ -48,13 +48,13 @@ class URLSessionHTTPClientTests : XCTestCase {
     
     func test_getFromURL_failsOnRequestError(){
         
-        URLProtocolStub.startInterceptingRequests() 
+        URLProtocolStub.startInterceptingRequests()
             let url = URL(string: "http://any-url.com")!
             let error = NSError(domain: "test", code: 1)
 //            let session = HTTPSessionSpy()
         
         //still need to stub the url with an error
-            URLProtocolStub.stub(url: url,error: error)
+        URLProtocolStub.stub(url: url,data:nil,response:nil,error: error)
         
             let sut = URLSessionHttpCLient()
         
@@ -82,11 +82,13 @@ private class URLProtocolStub : URLProtocol {
     private static var stubs = [URL:Stub]()
     
     private struct Stub {
+        let data : Data?
+        let response : URLResponse?
         let error :  Error?
     }
     
-   static func  stub(url : URL , error : Error? = nil){
-        stubs[url] = Stub( error: error)
+    static func  stub(url : URL , data: Data?, response : URLResponse?, error : Error? = nil){
+        stubs[url] = Stub( data: data, response:response, error: error)
     }
     
     static func startInterceptingRequests(){
@@ -111,6 +113,13 @@ private class URLProtocolStub : URLProtocol {
     override func startLoading() {
         guard let url = request.url, let stub =  URLProtocolStub.stubs[url] else { return }
         
+        if let data = stub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let response = stub.response {
+            client?.urlProtocol(self, didReceive: response,cacheStoragePolicy: .notAllowed)
+        }
         if let error = stub.error {
             client?.urlProtocol(self, didFailWithError: error)
         }
