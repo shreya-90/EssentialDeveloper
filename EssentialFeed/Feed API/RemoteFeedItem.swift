15c5cfb9079ad48b6cdab307f7c7eba5dfdf3f53
Shreya Pallan <shreya.90@gm>
2021-05-08 12:22:02 +0530

    Move RemoteFeedItem DTO to its own file

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Shreya Pallan on 08/05/21.
+//  Copyright © 2021 Shreya Pallan. All rights reserved.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem : Decodable {
+    
+     internal let id : UUID
+     internal let description : String?
+     internal let location : String?
+     internal let image : URL
+}
