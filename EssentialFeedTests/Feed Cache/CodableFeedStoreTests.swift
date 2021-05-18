58964b975ce5ca9e6bd65393503ba23a53eda4c3
Shreya Pallan <shreya.90@gm>
2021-05-18 14:47:10 +0530

    Make CodableFeedStore conform to FeedStore

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -10,7 +10,7 @@ import XCTest
 import EssentialFeed
 
 
-class CodableFeedStore {
+class CodableFeedStore: FeedStore {
     
     private struct Cache: Codable {
         let feed: [CodableFeedImage]
@@ -44,7 +44,7 @@ class CodableFeedStore {
         self.storeURL = storeURL
     }
     
-    func retrieve(completion : @escaping FeedStore.RetrievalCompletion){
+    func retrieve(completion : @escaping RetrievalCompletion){
         guard let data = try? Data(contentsOf: storeURL) else {
             return completion(.empty )
         }
@@ -58,7 +58,7 @@ class CodableFeedStore {
         }
     }
     
-    func insert(_ feed: [LocalFeedImage], timestamp : Date, completion : @escaping FeedStore.InsertionCompletion) {
+    func insert(_ feed: [LocalFeedImage], timestamp : Date, completion : @escaping InsertionCompletion) {
         
         do {
             let encoder = JSONEncoder()
@@ -73,7 +73,7 @@ class CodableFeedStore {
     }
     
     
-    func deleteCacheFeed(completion: @escaping FeedStore.DeletionCompletion){
+    func deleteCachedFeed(completion: @escaping DeletionCompletion){
         
         guard FileManager.default.fileExists(atPath: storeURL.path) else{
             return completion(nil)
@@ -261,7 +261,7 @@ class CodableFeedStoreTests: XCTestCase {
             let exp = expectation(description: "Wait for cache deletion")
             
             var deletionError: Error?
-            sut.deleteCacheFeed { receivedDeletionError in
+            sut.deleteCachedFeed { receivedDeletionError in
                 deletionError = receivedDeletionError
                 exp.fulfill()
             }
