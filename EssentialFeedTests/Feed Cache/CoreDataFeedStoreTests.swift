34ccfba859c6fdb34650f1f5b6855c4b0833a695
Shreya Pallan <shreya.90@gm>
2021-05-24 18:14:06 +0530

    Proved that CoreDataFeedStore side-effects run serially

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -72,12 +72,10 @@ class CoreDataFeedStoreTests: XCTestCase,FeedStoreSpecs {
         assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
-    func test_delete_deliversErrorOnDeletionError() {
-        
-    }
-    
     func test_storeSideEffects_runSerially() {
-        
+        let sut = makeSUT()
+
+        assertThatSideEffectsRunSerially(on: sut)
     }
     
     // - MARK: Helpers
